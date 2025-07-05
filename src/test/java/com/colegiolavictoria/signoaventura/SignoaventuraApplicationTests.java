eed5eb5e73d26e6df9deaf8d97c93db728703d8b
Dave-arch7 <davidbravomarin558@gmail.com>
2025-07-05 12:52:03 -0500

    subida de proyecto

---
+++ src/test/java/com/colegiolavictoria/signoaventura/SignoaventuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegiolavictoria.signoaventura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignoaventuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
